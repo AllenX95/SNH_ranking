00a95ae57f21aa5b790e2250ba43e8f1cabe8cdf
AllenX95 <xuleipku95@gmail.com>
2017-08-24 18:16:56 +0800

    Update Team SII.py

--- Team SII.py
+++ Team SII.py
@@ -1,1 +1,7 @@
 # ELO-based ranking code for Team SII
+import random
+sii = { "Chen Guanhui":1400, "Cheng Jue":1400, "Chen Si":1400, "Dai Meng":1400, "Jiang Yun":1400, "Kong Xiaoyin":1400, "Li Yuqi":1400,
+        "Mo Han":1400, "Qian Beiting":1400, "Qiu Xinyi":1400, "Sun Rui":1400, "Shen Zhilin":1400, "Wen Jingjie":1400, "Wu Zhehan":1400,
+        "Xu Chenchen":1400, "Xu Jiaqi":1400, "Xu Zixuan":1400, "Yuan Danni":1400, "Yuan Yuzhen":1400,"Zhao Ye":1400, "Zhang Yuge":1400,
+        "Lv Yi":1400, "Zhao Hanqian":1400, "Pan Yanqi":1400"}
+num_a = random.randint
